eca08436cb0c55c7c01430d0a4b64a6b4b241b9b
AchimNyaga <nyagaachim@gmail.com>
2024-04-26 13:41:19 +0300

    Added first pararaph and image

---
+++ build/haha.js
@@ -0,0 +1,10 @@
+<script> 
+    .js{
+    document.getElementById('menu-toggle').addEventListener('click', function()  {
+        var menu =
+        document.getElementById('menu');
+        menu.classList.toggle('hidden');
+    } ) 
+}
+
+</script>
